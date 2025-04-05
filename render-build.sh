422e05331f743d8c2e07a9d29023eda72209ccde
Darin Solyman <darinsolyman@Darins-MacBook-Pro.local>
2025-04-05 22:35:40 +0200

    Første opplasting av discord-bot

--- render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Installer systemavhengigheter for yt-dlp
-apt-get update
-apt-get install -y python3 python3-pip ffmpeg
-# Installer yt-dlp
-pip3 install yt-dlp
-# Kjør npm install for Node.js-avhengigheter
-npm install
